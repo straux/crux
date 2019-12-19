2a9ba6914e7e6a25a4306e63fbd05762452a467a
Jon Pither <jon.pither@gmail.com>
2019-12-19 14:07:47 +0000

    Simplifying instrument visitor

--- crux-test/test/crux/fixtures/instrument.clj
+++ crux-test/test/crux/fixtures/instrument.clj
@@ -2,14 +2,43 @@
   (:require [crux.index :as i]))
 
 (defprotocol Instrument
-  (instrument [i f visited]))
+  (instrument [i f]))
 
-(defn inst [depth visited i]
-  (instrument i depth visited))
+(extend-protocol Instrument
+  crux.index.NAryConstrainingLayeredVirtualIndex
+  (instrument [this f]
+    (let [this (update this :n-ary-index instrument f)]
+      (f this)))
 
-(defn ->instrumented-index [i f visited]
-  (let  [ii (or (and (instance? InstrumentedLayeredIndex i) i)
-                (get @visited i)
+  crux.index.NAryJoinLayeredVirtualIndex
+  (instrument [this f]
+    (let [this (update this :unary-join-indexes (fn [indexes] (doall (map #(instrument % f) indexes))))]
+      (f this)))
+
+  crux.index.UnaryJoinVirtualIndex
+  (instrument [this f]
+    (let [this (update this :indexes (fn [indexes] (doall (map #(instrument % f) indexes))))]
+      (f this)))
+
+  crux.index.BinaryJoinLayeredVirtualIndex
+  (instrument [^crux.index.BinaryJoinLayeredVirtualIndex this f]
+    (let [state ^crux.index.BinaryJoinLayeredVirtualIndexState (.state this)
+          [lhs rhs] (doall (map #(instrument % f) (.indexes state)))]
+      (set! (.indexes state) [lhs rhs])
+      (f this)))
+
+  crux.index.RelationVirtualIndex
+  (instrument [^crux.index.RelationVirtualIndex this f]
+    (let [state ^crux.index.RelationIteratorsState (.state this)]
+      (set! (.indexes state) (mapv #(instrument % f) (.indexes state)))
+      (f this)))
+
+  Object
+  (instrument [this f]
+    (f this)))
+
+(defn ->instrumented-index [visited f i]
+  (let  [ii (or (get @visited i)
                 (let [ii (f visited i)]
                   (swap! visited assoc i ii)
                   ii))]
@@ -17,40 +46,7 @@
       (assoc ii :name (:name i))
       ii)))
 
-(extend-protocol Instrument
-  crux.index.NAryConstrainingLayeredVirtualIndex
-  (instrument [this f visited]
-    (let [this (update this :n-ary-index (partial inst f visited))]
-      (->instrumented-index this f visited)))
-
-  crux.index.NAryJoinLayeredVirtualIndex
-  (instrument [this f visited]
-    (let [this (update this :unary-join-indexes (fn [indexes] (map (partial inst f visited) indexes)))]
-      (->instrumented-index this f visited)))
-
-  crux.index.UnaryJoinVirtualIndex
-  (instrument [this f visited]
-    (or (get @visited this)
-        (let [this (update this :indexes (fn [indexes] (map (partial inst f visited) indexes)))]
-          (->instrumented-index this f visited))))
-
-  crux.index.BinaryJoinLayeredVirtualIndex
-  (instrument [^crux.index.BinaryJoinLayeredVirtualIndex this f visited]
-    (let [state ^crux.index.BinaryJoinLayeredVirtualIndexState (.state this)
-          [lhs rhs] (doall (map (partial inst f visited) (.indexes state)))]
-      (set! (.indexes state) [lhs rhs])
-      (->instrumented-index this f visited)))
-
-  crux.index.RelationVirtualIndex
-  (instrument [^crux.index.RelationVirtualIndex this f visited]
-    (let [state ^crux.index.RelationIteratorsState (.state this)]
-      (set! (.indexes state) (mapv (partial inst f visited) (.indexes state)))
-      (->instrumented-index this f visited)))
-
-  Object
-  (instrument [this f visited]
-    (->instrumented-index this f visited)))
-
 (def original-layered-idx->seq i/layered-idx->seq)
 (defn instrumented-layered-idx->seq [f idx]
-  (original-layered-idx->seq (instrument idx f (atom {}))))
+  (let [f (partial ->instrumented-index (atom {}) f)]
+    (original-layered-idx->seq (instrument idx f))))

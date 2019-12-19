b7d4ce2c122d09fef0879636271c989df5ee64ba
Jon Pither <jon.pither@gmail.com>
2019-12-19 14:27:45 +0000

    Add childen to graph

--- crux-test/test/crux/fixtures/query_plan.clj
+++ crux-test/test/crux/fixtures/query_plan.clj
@@ -5,11 +5,46 @@
             [loom.graph :as g]
             [loom.io :as lio]))
 
+(defprotocol Children
+  (children [i]))
+
+(extend-protocol Children
+  crux.index.NAryConstrainingLayeredVirtualIndex
+  (children [this]
+    (:n-ary-index this))
+
+  crux.index.NAryJoinLayeredVirtualIndex
+  (children [this]
+    (:unary-join-indexes this))
+
+  crux.index.UnaryJoinVirtualIndex
+  (children [this]
+    (:indexes this))
+
+  crux.index.BinaryJoinLayeredVirtualIndex
+  (children [^crux.index.BinaryJoinLayeredVirtualIndex this]
+    (let [state ^crux.index.BinaryJoinLayeredVirtualIndexState (.state this)]
+      (.indexes state)))
+
+  crux.index.RelationVirtualIndex
+  (children [^crux.index.RelationVirtualIndex this]
+    (let [state ^crux.index.RelationIteratorsState (.state this)]
+      (.indexes state)))
+
+  Object
+  (children [this]
+    nil))
+
 (comment
   (lio/view (g/add-nodes (g/graph) :a :b)))
 
 (defn loom-instrumenter [g visited i]
   (swap! g g/add-nodes (t/index-name i))
+  (swap! g (fn [g] (->> i
+                        children
+                        (map t/index-name)
+                        (map (partial vector (t/index-name i)))
+                        (apply g/add-edges g))))
   i)
 
 (defn instrument-layered-idx->seq [idx]

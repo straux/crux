368a0d0882ab0b253d5cfcb3c1e932318e1b3ca7
Jon Pither <jon.pither@gmail.com>
2019-12-19 13:23:00 +0000

    Moving depth into a custom instrumenter

--- crux-test/test/crux/fixtures/instrument.clj
+++ crux-test/test/crux/fixtures/instrument.clj
@@ -126,7 +126,7 @@
     (db/max-depth i)))
 
 (defprotocol Instrument
-  (instrument [i depth visited]))
+  (instrument [i f visited]))
 
 (defn inst [depth visited i]
   (instrument i depth visited))
@@ -135,10 +135,13 @@
   (when (instance? crux.index.BinaryJoinLayeredVirtualIndex i)
     (first (shuffle (clojure.set/difference (set (keys ansi-colors)) (set (map :color (vals @visited))))))))
 
-(defn ->instrumented-index [i depth visited]
+(defn trace-instrumenter [depth visited i]
+  (InstrumentedLayeredIndex. i depth (atom 0) (get-color i visited)))
+
+(defn ->instrumented-index [i f visited]
   (let  [ii (or (and (instance? InstrumentedLayeredIndex i) i)
                 (get @visited i)
-                (let [ii (InstrumentedLayeredIndex. i depth (atom 0) (get-color i visited))]
+                (let [ii (f visited i)]
                   (swap! visited assoc i ii)
                   ii))]
     (if (instance? crux.index.BinaryJoinLayeredVirtualIndex i)
@@ -147,41 +150,42 @@
 
 (extend-protocol Instrument
   crux.index.NAryConstrainingLayeredVirtualIndex
-  (instrument [this depth visited]
-    (let [this (update this :n-ary-index (partial inst depth visited))]
-      (->instrumented-index this depth visited)))
+  (instrument [this f visited]
+    (let [this (update this :n-ary-index (partial inst f visited))]
+      (->instrumented-index this f visited)))
 
   crux.index.NAryJoinLayeredVirtualIndex
-  (instrument [this depth visited]
-    (let [this (update this :unary-join-indexes (fn [indexes] (map (partial inst depth visited) indexes)))]
-      (->instrumented-index this depth visited)))
+  (instrument [this f visited]
+    (let [this (update this :unary-join-indexes (fn [indexes] (map (partial inst f visited) indexes)))]
+      (->instrumented-index this f visited)))
 
   crux.index.UnaryJoinVirtualIndex
-  (instrument [this depth visited]
+  (instrument [this f visited]
     (or (get @visited this)
-        (let [this (update this :indexes (fn [indexes] (map (partial inst depth visited) indexes)))]
-          (->instrumented-index this depth visited))))
+        (let [this (update this :indexes (fn [indexes] (map (partial inst f visited) indexes)))]
+          (->instrumented-index this f visited))))
 
   crux.index.BinaryJoinLayeredVirtualIndex
-  (instrument [^crux.index.BinaryJoinLayeredVirtualIndex this depth visited]
+  (instrument [^crux.index.BinaryJoinLayeredVirtualIndex this f visited]
     (let [state ^crux.index.BinaryJoinLayeredVirtualIndexState (.state this)
-          [lhs rhs] (doall (map (partial inst depth visited) (.indexes state)))]
+          [lhs rhs] (doall (map (partial inst f visited) (.indexes state)))]
       (set! (.indexes state) [lhs rhs])
-      (->instrumented-index this depth visited)))
+      (->instrumented-index this f visited)))
 
   crux.index.RelationVirtualIndex
-  (instrument [^crux.index.RelationVirtualIndex this depth visited]
+  (instrument [^crux.index.RelationVirtualIndex this f visited]
     (let [state ^crux.index.RelationIteratorsState (.state this)]
-      (set! (.indexes state) (mapv (partial inst depth visited) (.indexes state)))
-      (->instrumented-index this depth visited)))
+      (set! (.indexes state) (mapv (partial inst f visited) (.indexes state)))
+      (->instrumented-index this f visited)))
 
   Object
-  (instrument [this depth visited]
-    (->instrumented-index this depth visited)))
+  (instrument [this f visited]
+    (->instrumented-index this f visited)))
 
 (def original-layered-idx->seq i/layered-idx->seq)
 (defn instrument-layered-idx->seq [idx]
-  (original-layered-idx->seq (instrument idx (atom {:seek 0 :next 0}) (atom {}))))
+  (let [f (partial trace-instrumenter (atom {:seek 0 :next 0}))]
+    (original-layered-idx->seq (instrument idx f (atom {})))))
 
 (defmacro with-instrumentation [& form]
   `(with-redefs [i/layered-idx->seq instrument-layered-idx->seq]
